f69836683ff4caf894454742492930c2c0780a87
neroexel <zaitov2017@gmail.com>
2026-03-17 14:48:12 +0500

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -7501,7 +7501,13 @@ ElementsTable.Discord = (function()
             ThemeTag = { BackgroundColor3 = "Element", TextColor3 = "Accent" }
         }, {
             New("UICorner", {CornerRadius = UDim.new(0, 6)}),
-            New("ImageLabel", {Image = "rbxassetid://10042131950", Size = UDim2.fromOffset(20,20), Position = UDim2.new(0,10,0.5,0), AnchorPoint = Vector2.new(0,0.5)})
+            New("ImageLabel", {
+    Image = Library:GetIcon("discord") or "rbxassetid://10042131950",
+    Size = UDim2.fromOffset(20,20),
+    Position = UDim2.new(0,10,0.5,0),
+    AnchorPoint = Vector2.new(0,0.5),
+    BackgroundTransparency = 1
+})
         })
 
         Button.MouseButton1Click:Connect(function()
