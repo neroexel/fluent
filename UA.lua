0400797d4dd1fd24c03f30d38fc3945da9d5a330
neroexel <zaitov2017@gmail.com>
2026-04-01 14:24:14 +0500

    Update UA.lua

--- UA.lua
+++ UA.lua
@@ -18,7 +18,7 @@ local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 local TextService = game:GetService("TextService")
 local Workspace = game:GetService("Workspace")
-local HttpService = game:GetService("HttpService")
+local httpService = game:GetService("HttpService")
 
 local LocalPlayer = Players.LocalPlayer
 local Camera = Workspace.CurrentCamera
@@ -3437,7 +3437,10 @@ Components.Button = (function()
 			Button.Title,
 		})
 		local Motor, SetTransparency = Creator.SpringMotor(1, Button.HoverFrame, "BackgroundTransparency", DialogCheck)
+local LastHover = 0
 Creator.AddSignal(Button.Frame.MouseEnter, function()
+	if tick() - LastHover < 0.016 then return end
+	LastHover = tick()
 	SetTransparency(0.97)
 end)
 		Creator.AddSignal(Button.Frame.MouseLeave, function()
